a1bc768a603ed415847baf705534f87afc91e463
firasaj <77369751+xv786vx@users.noreply.github.com>
2025-01-18 16:59:33 -0500

    tailwindcss works now
    Co-authored-by: Danial Suhail <Danial-Suhail@users.noreply.github.com>

--- frontend/campus-chronicles/src/App.tsx
+++ frontend/campus-chronicles/src/App.tsx
@@ -1,15 +1,24 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 import  React from 'react'
+import './index.css'
+
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <div className="w-screen h-screen bg-red-500">
+      <header className="text-center">
+        <h1 className="pt-32 text-7xl font-bold mb-8">Campus Chronicles</h1>
+        <SignedOut>
+          <SignInButton>
+            <button className="mt-48 px-24 py-16 bg-neutral-500 text-white text-4xl rounded-lg hover:bg-blue-600">
+              Sign In dude
+            </button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+    </div>
   )
 }
