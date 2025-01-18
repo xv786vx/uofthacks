cae8fccefbd8bef7c15c408ed68fb35de817fe40
firasaj <77369751+xv786vx@users.noreply.github.com>
2025-01-18 16:26:31 -0500

    fixed .env error

--- frontend/campus-chronicles/src/main.tsx
+++ frontend/campus-chronicles/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
+
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
--- frontend/campus-chronicles/src/vite-env.d.ts
+++ frontend/campus-chronicles/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_CLERK_PUBLISHABLE_KEY: string;
+    // add other environment variables here...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
