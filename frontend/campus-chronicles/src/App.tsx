1d7fca12555d3e50dde9308a1b7fa1a64e395a72
firasaj <77369751+xv786vx@users.noreply.github.com>
2025-01-18 22:01:52 -0500

    fixed thing

--- frontend/campus-chronicles/src/App.tsx
+++ frontend/campus-chronicles/src/App.tsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/clerk-react'
 import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './index.css'
 import HomePage from './HomePage';
 
